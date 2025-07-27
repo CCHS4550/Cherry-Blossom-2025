390c6e918893ff123fbcbac9c411587bf3017c7a
RuhanRajasthan <rsharma@cherrycreekschools.org>
2025-07-27 13:17:09 -0600

    micron of work on pneumatics

---
+++ src/main/java/frc/robot/Subsystems/Turret/Pneumatics/PneumaticsIO.java
@@ -0,0 +1,22 @@
+package frc.robot.Subsystems.Turret.Pneumatics;
+
+import org.littletonrobotics.junction.AutoLog;
+
+
+public interface PneumaticsIO {
+    @AutoLog
+    public static class PneumaticsIOInputs{
+        public boolean connected = false;
+        public double pressurePSI = 0.0;
+        public double pressureChangePSIPerSecond = 0.0;
+
+        public double [] pressureTimestamps = new double [] {};
+        public double [] pressureValues = new double [] {};
+
+        public double desiredPressure = 0.0;
+        public double desiredPressurePerSecond = 0.0;
+    }
+
+    default void updateInputs (PneumaticsIOInputs inputs) {}
+
+}
