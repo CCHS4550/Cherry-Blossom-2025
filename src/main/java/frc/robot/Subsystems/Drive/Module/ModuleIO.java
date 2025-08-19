0a4aa21bfe6b1796ecdedc102ecc31df8946fc30
holyb3anS <144182597+holyb3anS@users.noreply.github.com>
2025-08-19 17:56:22 -0600

    added comments for modules

--- src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIO.java
+++ src/main/java/frc/robot/Subsystems/Drive/Module/ModuleIO.java
@@ -3,21 +3,26 @@ package frc.robot.Subsystems.Drive.Module;
 import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
 
+//creates an interface for hardware to fully define
 public interface ModuleIO {
+  // automatically generates a class using clonable that logs the following variables with advantage scope
   @AutoLog
   public static class ModuleIOInputs {
+    // tracking variables for the drive motor
     public boolean driveConnected = false;
     public double drivePositionRad = 0.0;
     public double driveVelocityRadPerSec = 0.0;
     public double driveAppliedVolts = 0.0;
     public double driveCurrentAmps = 0.0;
 
+    // tracking variables for the turn motor
     public boolean turnConnected = false;
     public Rotation2d turnPosition = new Rotation2d();
     public double turnVelocityRadPerSec = 0.0;
     public double turnAppliedVolts = 0.0;
     public double turnCurrentAmps = 0.0;
 
+    // tracking the odometry queues
     public double[] odometryTimestamps = new double[] {};
     public double[] odometryDrivePositionsRad = new double[] {};
     public Rotation2d[] odometryTurnPositions = new Rotation2d[] {};
