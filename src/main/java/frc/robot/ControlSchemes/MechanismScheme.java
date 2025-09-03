b9910c41219e1745fe0c931f0f9b375a385ceea2
RuhanRajasthan <144182793+RuhanRajasthan@users.noreply.github.com>
2025-09-03 10:18:35 -0600

    added more logging to MechanismScheme

--- src/main/java/frc/robot/ControlSchemes/MechanismScheme.java
+++ src/main/java/frc/robot/ControlSchemes/MechanismScheme.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Subsystems.Superstructure;
 import frc.robot.Subsystems.Superstructure.wantedState;
+import org.littletonrobotics.junction.Logger;
+
 
 /** how our controller or button board interacts with the superstructure */
 public class MechanismScheme {
@@ -22,57 +24,89 @@ public class MechanismScheme {
         .povUp()
         .onTrue(
             new InstantCommand(
-                () -> superstructure.setWantedState(wantedState.ELEVATION_OPENLOOP_UP)));
+                () -> {
+                    Logger.recordOutput("Elevation Open Loop Up Command", true);
+                    superstructure.setWantedState(wantedState.ELEVATION_OPENLOOP_UP));
+                });
     controller
         .povUp()
-        .onFalse(new InstantCommand(() -> superstructure.setWantedState(wantedState.IDLE)));
+        .onFalse(new InstantCommand(() ->{
+            Logger.recordOutput("Elevation Open Loop Up Command", false);
+            superstructure.setWantedState(wantedState.IDLE));
+        });
 
     // go down on DPAD DOWN
     controller
         .povDown()
         .onTrue(
             new InstantCommand(
-                () -> superstructure.setWantedState(wantedState.ELEVATION_OPENLOOP_DOWN)));
+                () -> {
+                Logger.recordOutput("Elevation Open Loop Down Command", true);
+                superstructure.setWantedState(wantedState.ELEVATION_OPENLOOP_DOWN));
+                });
     controller
         .povDown()
-        .onFalse(new InstantCommand(() -> superstructure.setWantedState(wantedState.IDLE)));
+        .onFalse(new InstantCommand(() -> {
+            Logger.recordOutput("Elevation Open Loop Down Command", false);
+            superstructure.setWantedState(wantedState.IDLE));
+        });
 
     // go right on DPAD right
     controller
         .povRight()
         .onTrue(
             new InstantCommand(
-                () -> superstructure.setWantedState(wantedState.ROTATION_OPENLOOP_CLOCKWISE)));
+                () -> {
+                    Logger.recordOutput("Rotation Open Loop Clockwise Command", true);
+                    superstructure.setWantedState(wantedState.ROTATION_OPENLOOP_CLOCKWISE));
+                });
     controller
         .povRight()
-        .onFalse(new InstantCommand(() -> superstructure.setWantedState(wantedState.IDLE)));
+        .onFalse(new InstantCommand(() -> {
+            Logger.recordOutput("Rotation Open Loop Clockwise Command", false);
+            superstructure.setWantedState(wantedState.IDLE));
+        });
 
     // go left on DPAD left
     controller
         .povLeft()
         .onTrue(
             new InstantCommand(
-                () ->
-                    superstructure.setWantedState(wantedState.ROTATION_OPENLOOP_COUNTERCLOCKWISE)));
+                () ->{
+                    Logger.recordOutput("Elevation Open Loop Counterclockwise Command", true);
+                    superstructure.setWantedState(wantedState.ROTATION_OPENLOOP_COUNTERCLOCKWISE));
+                });
     controller
         .povLeft()
-        .onFalse(new InstantCommand(() -> superstructure.setWantedState(wantedState.IDLE)));
+        .onFalse(new InstantCommand(() -> {
+            Logger.recordOutput("Elevation Open Loop Counterclockwise Command", false);
+            superstructure.setWantedState(wantedState.IDLE));
+        });
 
     // shoot one on right trigger
     controller
         .rightTrigger()
-        .onTrue(new InstantCommand(() -> superstructure.setWantedState(wantedState.SHOOT_ONE)));
+        .onTrue(new InstantCommand(() -> {
+            Logger.recordOutput("Shoot One Command", true);
+            superstructure.setWantedState(wantedState.SHOOT_ONE));
+        });
 
     // shoot all on both press
     controller
         .rightTrigger()
         .and(controller.leftTrigger())
-        .onTrue(new InstantCommand(() -> superstructure.setWantedState(wantedState.SHOOT_ALL)));
+        .onTrue(new InstantCommand(() -> {
+            Logger.recordOutput("Shoot All Command", true);
+            superstructure.setWantedState(wantedState.SHOOT_ALL));
+        });
 
     controller
         .a()
         .onTrue(
             new InstantCommand(
-                () -> superstructure.setWantedState(wantedState.ROTATE_60_DEGREES_BOT_ORIENTED)));
+                () -> {
+                    Logger.recordOutput("Rotate 60 Degrees Command", true);
+                    superstructure.setWantedState(wantedState.ROTATE_60_DEGREES_BOT_ORIENTED));
+                });
   }
 }
